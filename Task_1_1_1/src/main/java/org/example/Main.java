411a043f2b466a54e05e62bc5c97d083919d80fa
Karloph <v.labusov@g.nsu.ru>
2023-10-27 11:40:06 +0700

    Change name of functions

--- Task_1_1_1/src/main/java/org/example/Main.java
+++ Task_1_1_1/src/main/java/org/example/Main.java
@@ -1,40 +1,40 @@
 package org.example;
 
 public class Main {
-    private static void SiftUp(int len, int[] newArray) { //отсеивание вниз
+    private static void siftUp(int len, int[] newArray) { //отсеивание вниз
         int x;
         if ((len - 1) / 2 >= 0 && newArray[len] < newArray[(len - 1) / 2]) {
             x = newArray[len];
             newArray[len] = newArray[(len - 1) / 2];
             newArray[(len - 1) / 2] = x;
-            SiftUp((len - 1) / 2, newArray);
+            siftUp((len - 1) / 2, newArray);
         }
     }
 
-    private static void SiftDown(int current, int len, int[] newArray) { //отсеивание вверх
+    private static void siftDown(int current, int len, int[] newArray) { //отсеивание вверх
         int x;
         if (current * 2 + 2 < len && newArray[current * 2 + 1] < newArray[current] && newArray[current * 2 + 2] < newArray[current]) {
             if (newArray[current * 2 + 1] < newArray[current * 2 + 2]) {
                 x = newArray[current * 2 + 1];
                 newArray[current * 2 + 1] = newArray[current];
                 newArray[current] = x;
-                SiftDown(current * 2 + 1, len, newArray);
+                siftDown(current * 2 + 1, len, newArray);
             } else {
                 x = newArray[current * 2 + 2];
                 newArray[current * 2 + 2] = newArray[current];
                 newArray[current] = x;
-                SiftDown(current * 2 + 2, len, newArray);
+                siftDown(current * 2 + 2, len, newArray);
             }
         } else if (current * 2 + 1 < len && newArray[current * 2 + 1] < newArray[current]) {
             x = newArray[current * 2 + 1];
             newArray[current * 2 + 1] = newArray[current];
             newArray[current] = x;
-            SiftDown(current * 2 + 1, len, newArray);
+            siftDown(current * 2 + 1, len, newArray);
         } else if (current * 2 + 2 < len && newArray[current * 2 + 2] < newArray[current]) {
             x = newArray[current * 2 + 2];
             newArray[current * 2 + 2] = newArray[current];
             newArray[current] = x;
-            SiftDown(current * 2 + 2, len, newArray);
+            siftDown(current * 2 + 2, len, newArray);
         }
     }
 
@@ -43,14 +43,14 @@ public class Main {
         int[] newTree = new int[size];
         for (int i = 0; i < size; i++) { // записывание в новый
             newTree[i] = list[i];
-            SiftUp(i, newTree);
+            siftUp(i, newTree);
         }
         int x;
         for (int i = size - 1; i >= 0; i--) { // отсев наименьшего
             x = newTree[0];
             newTree[0] = newTree[i];
             newTree[i] = x;
-            SiftDown(0, i, newTree);
+            siftDown(0, i, newTree);
         }
         for (int i = 0; i < size; i++) // записываем полученный в который дали на входе
             list[i] = newTree[size - 1 - i];
